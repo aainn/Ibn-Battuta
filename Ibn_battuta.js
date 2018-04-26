c9e91888e0fc31c0487f3aa79594fe1a8979c5fb
kamalakil <38329554+kamalakil@users.noreply.github.com>
2018-04-26 17:33:02 +0200

    Done for me

--- Ibn_battuta.js
+++ Ibn_battuta.js
@@ -3,7 +3,6 @@ console.log('Starting leaflet test');
 
 var countryDetails = {};
 
-
 var ajax = new XMLHttpRequest();
 ajax.open("GET", "https://restcountries.eu/rest/v2/all", true);
 ajax.onload = function () {
@@ -17,11 +16,108 @@ ajax.onload = function () {
         countryDataList.innerHTML = `
             ${name}`;
         namegeter.appendChild(countryDataList);
-
     }
     console.log(list);
 };
 ajax.send();
+
+var urlInfo;
+function searchinfo() {
+    var infoName = document.getElementById('names').value;
+    if (infoName.indexOf(" ") > 0) {
+        if (infoName === "Åland Islands"    || infoName === "Bosnia and Herzegovina"|| infoName === "Bouvet Island"
+            || infoName === "British Indian Ocean Territory"                        || infoName === "Antigua and Barbuda"               || infoName === "United States Minor Outlying Islands"
+            || infoName === "British Indian Ocean Territory"                        || infoName === "Burkina Faso"                      || infoName === "Cayman Islands"                        || infoName === "Central African Republic"  || infoName === "Christmas Island"  || infoName === "Cocos (Keeling) Islands"
+            || infoName === "Cook Islands"  || infoName === "Costa Rica"            || infoName === "Czech Republic"                    || infoName === "Dominican Republic"                    || infoName === "El Salvador"               || infoName === "Equatorial Guinea" || infoName === "Faroe Islands"
+            || infoName === "French Guiana" || infoName === "French Polynesia"      || infoName === "Heard Island and McDonald Islands" || infoName === "Holy See"                              || infoName === "Hong Kong"                 || infoName === "Isle of Man"       || infoName === "Marshall Islands"
+            || infoName === "New Caledonia" || infoName === "New Zealand"           || infoName === "Norfolk Island"                    || infoName === "Northern Mariana Islands"              || infoName === "Papua New Guinea"          || infoName === "Saint Barthélemy"  || infoName === "Sao Tome and Principe"
+            || infoName === "Saint Helena, Ascension and Tristan da Cunha"          || infoName === "Saint Lucia"                       || infoName === "Saint Vincent and the Grenadines"      || infoName === "Saint Kitts and Nevis"     || infoName === "San Marino"        || infoName === "Saudi Arabia"
+            || infoName === "Sierra Leone"  || infoName === "Solomon Islands"       || infoName === "South Africa"                      || infoName === "South Georgia and the South Sandwich Islands"                                      || infoName === "South Sudan"       || infoName === "Sri Lanka"
+            || infoName === "Svalbard and Jan Mayen"                                || infoName === "Trinidad and Tobago"               || infoName === "Turks and Caicos Islands"              || infoName === "United Arab Emirates"      || infoName === "Wallis and Futuna" || infoName === "Western Sahara"
+            || infoName === "American Samoa"           
+        ) {
+            infoName = infoName.replace(/\s+/g, '_');
+        } else if (infoName === "Virgin Islands (British)") {
+            infoName = "British_Virgin_Islands";
+        } else if (infoName === "Congo (Democratic Republic of the)") {
+            infoName = "Democratic_Republic_of_the_Congo";
+        } else if (infoName === "Virgin Islands (U.S.)") {
+            infoName = "United_States_Virgin_Islands";
+        } else if (infoName === "Lao People's Democratic Republic") {
+            infoName = "Laos";
+        } else if (infoName === "Macedonia (the former Yugoslav Republic of)") {
+            infoName = "Republic_of_Macedonia";
+        } else if (infoName === "Côte d'Ivoire") {
+            infoName = "Ivory_Coast";
+        } else if (infoName === "Falkland Islands (Malvinas)") {
+            infoName = "Falkland_Islands";
+        } else if (infoName === "French Southern Territories") {
+            infoName = "French_Southern_and_Antarctic_Lands";
+        } else if (infoName === "Korea (Democratic People's Republic of)") {
+            infoName = "North_Korea";
+        } else if (infoName === "Palestine, State of") {
+            infoName = "State_of_Palestine";
+        } else if (infoName === "Republic of Kosovo") {
+            infoName = "Kosovo";
+        } else if (infoName === "Russian Federation") {
+            infoName = "Russia";
+        } else if (infoName === "Saint Martin (French part)") {
+            infoName = "Saint_Martin";
+        } else if (infoName === "Sint Maarten (Dutch part)") {
+            infoName = "Sint_Maarten";
+        } else if (infoName === "Korea (Republic of)") {
+            infoName = "South_Korea";
+        } else if (infoName === "Syrian Arab Republic") {
+            infoName = "Syria";
+        } else if (infoName === "United Kingdom of Great Britain and Northern Ireland") {
+            infoName = "United_Kingdom_of_Great_Britain_and_Ireland";
+        } else if (infoName === "United States of America") {
+            infoName = "United_States";
+        }
+        else {
+            infoName = infoName.substr(0, infoName.indexOf(" "));
+            infoName = infoName.replace(/[^a-zA-Z ]/g, "");
+        }
+    } else { var infoName = infoName }
+
+
+    urlInfo = ('http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=')
+    urlInfo += `${infoName}&callback=?`;
+
+    console.log(urlInfo);
+    infoElement();
+}
+
+function infoElement() {
+
+    $.ajax({
+        type: "GET",
+        url: urlInfo,
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
+
+            var markup = data.parse.text["*"];
+            var info = $('<div></div>').html(markup);
+
+            info.find('a').each(function () { $(this).replaceWith($(this).html()); });
+
+            info.find('sup').remove();
+            info.find('srcset').remove();
+
+            info.find('.mw-ext-cite-error').remove();
+
+            $('#article').html($(info).find('p'));
+
+        },
+        error: function (errorMessage) {
+        }
+    });
+
+};
+
+
 var input = document.getElementById("names");
 input.addEventListener("keyup", function (event) {
     event.preventDefault();
@@ -32,7 +128,22 @@ input.addEventListener("keyup", function (event) {
 //To get the name of the country in the console Log
 function submit() {
     document.getElementById("flag").innerHTML = "";
+    document.getElementById("article").innerHTML = "";
+    document.getElementById("capitalCity").innerHTML = "";
+    document.getElementById("language").innerHTML = "";
+    document.getElementById("currency").innerHTML = "";
+    document.getElementById("timezone").innerHTML = "";
+    document.getElementById("region").innerHTML = "";
+    document.getElementById("nameOfCountry").innerHTML = "";
     countryName();
+    capitalCity();
+    language()
+    currency()
+    timezone()
+    region()
+    nameOfCountry()
+    searchinfo()
+    latlnggeo();
 }
 function countryName() {
 
@@ -44,9 +155,76 @@ function countryName() {
     flagEl.src = flag;
     flagEl.alt = nameValue;
     infoEl.appendChild(flagEl);
-    mymap.removeLayer(marker)
-    latlnggeo();
+    mymap.removeLayer(marker) 
+    
 }
+function capitalCity() {
+
+    var capitalCity = document.getElementById('capitalCity');
+    var capitalName = document.getElementById('names').value;
+    var capital = countryDetails[capitalName].capital;
+    var capitalEl = document.createElement('p');
+    capitalEl.className = 'capital';
+    capitalEl.innerHTML=`
+    ${capital}`
+    capitalCity.appendChild(capitalEl);
+  }
+  function language() {
+
+    var languageID = document.getElementById('language');
+    var languageName = document.getElementById('names').value;
+    var language = countryDetails[languageName].languages[0].name;
+    var languageEl = document.createElement('p');
+    languageEl.className = 'language';
+    languageEl.innerHTML=`
+    ${language}`
+    languageID.appendChild(languageEl);
+  }
+  function currency() {
+
+    var currencyId = document.getElementById('currency');
+    var currencyName = document.getElementById('names').value;
+    var currency = countryDetails[currencyName].currencies[0].name;
+    var currencyEl = document.createElement('p');
+    currencyEl.className = 'currency';
+    currencyEl.innerHTML=`
+    ${currency}`
+    currencyId.appendChild(currencyEl);
+  }
+  function timezone() {
+
+    var timezoneId = document.getElementById('timezone');
+    var timezoneName = document.getElementById('names').value;
+    var timezone = countryDetails[timezoneName].timezones[0];
+    var timezoneEl = document.createElement('p');
+    timezoneEl.className = 'currency';
+    timezoneEl.innerHTML=`
+    ${timezone}`
+    timezoneId.appendChild(timezoneEl);
+  }
+
+  function region() {
+
+    var regionId = document.getElementById('region');
+    var regionName = document.getElementById('names').value;
+    var region = countryDetails[regionName].region;
+    var regionEl = document.createElement('p');
+    regionEl.className = 'currency';
+    regionEl.innerHTML=`
+    ${region}`
+    regionId.appendChild(regionEl);
+  }
+  function nameOfCountry() {
+
+    var countryNameId = document.getElementById('nameOfCountry');
+    var countryName = document.getElementById('names').value;
+    var country = countryDetails[countryName].name;
+    var countryNameEl = document.createElement('h1');
+    countryNameEl.id = 'NameOftheCountry';
+    countryNameEl.innerHTML=`
+    ${country}`
+    countryNameId.appendChild(countryNameEl);
+  }
 
 var myLocation = [51, 9];
 
@@ -72,38 +250,3 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: mapBoxToken
 }).addTo(mymap);
-
-
-
-
-
-
-
-
-
-
-
-// var marker = L.marker(myLocation).addTo(mymap);
-
-// var circleLocation = [myLocation[0], myLocation[1] + 0.01];
-// var circle = L.circle(circleLocation, {
-//     color: 'red',
-//     fillColor: '#f03',
-//     fillOpacity: 0.5,
-// //     radius: 500
-// }).addTo(mymap);
-
-// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-
-// circle.bindPopup(`I'm a circle.`);
-
-
-// var popup = L.popup();  
-// function onMapClick(e) {
-//     popup
-//         .setLatLng(e.latlng)
-//         .setContent("You clicked the map at " + e.latlng.toString())
-//         .openOn(mymap);
-// }
-
-// mymap.on('click', onMapClick);
